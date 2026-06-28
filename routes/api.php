9a310dd1d5d99b10e3b8f8047645619b685aac63
apriansyahrs <apriansyahrizs@gmail.com>
2026-06-28 20:46:08 +0700

    style: format helpdesk API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AiHelpdeskActionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AiHelpdeskActionController;
 
 /*
 |--------------------------------------------------------------------------
